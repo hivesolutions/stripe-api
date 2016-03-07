816fe53c7e465d9a3e3c8dea265febe40da17893
joamag <joamag@gmail.com>
2016-03-07 23:02:00 +0000

    initial support for examples

---
+++ src/examples/app.py
@@ -0,0 +1,64 @@
+##!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Stripe API
+# Copyright (c) 2008-2016 Hive Solutions Lda.
+#
+# This file is part of Hive Stripe API.
+#
+# Hive Stripe API is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
+#
+# Hive Stripe API is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License along with
+# Hive Stripe API. If not, see <http://www.apache.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2016 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "Apache License, Version 2.0"
+""" The license for the module """
+
+import appier
+
+from . import base
+
+class StripeApp(appier.WebApp):
+
+    def __init__(self):
+        appier.WebApp.__init__(self, name = "stripe")
+
+    @appier.route("/", "GET")
+    def index(self):
+        return self.balance()
+
+    @appier.route("/balance", "GET")
+    def balance(self):
+        api = self.get_api()
+        balance = api.get_balance()
+        return balance
+
+    def get_api(self):
+        return base.get_api()
+
+if __name__ == "__main__":
+    app = StripeApp()
+    app.serve()
