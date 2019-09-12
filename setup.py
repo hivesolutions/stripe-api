9e54889ccf68e6ff0501e5c8ae845a0f05e74afd
joamag <joamag@hive.pt>
2019-09-12 17:16:55 +0100

    New version 0.2.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "stripe_api",
-    version = "0.1.16",
+    version = "0.2.0",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Stripe API Client",
