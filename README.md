88b95497c185982fd67a406ea7f2ee6082815e16
João Magalhães <joamag@gmail.com>
2025-03-01 19:02:48 +0000

    chore: migrate from Travis CI to GitHub Actions

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Stripe API is currently licensed under the [Apache License, Version 2.0](http://
 
 ## Build Automation
 
-[![Build Status](https://app.travis-ci.com/hivesolutions/stripe-api.svg?branch=master)](https://travis-ci.com/github/hivesolutions/stripe-api)
+[![Build Status](https://github.com/hivesolutions/stripe-api/workflows/Main%20Workflow/badge.svg)](https://github.com/hivesolutions/stripe-api/actions)
 [![Coverage Status](https://coveralls.io/repos/hivesolutions/stripe-api/badge.svg?branch=master)](https://coveralls.io/r/hivesolutions/stripe-api?branch=master)
 [![PyPi Status](https://img.shields.io/pypi/v/stripe-api.svg)](https://pypi.python.org/pypi/stripe-api)
 [![License](https://img.shields.io/badge/license-Apache%202.0-blue.svg)](https://www.apache.org/licenses/)
---
+++ src/stripe/test/base.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Stripe API
+# Copyright (c) 2008-2025 Hive Solutions Lda.
+#
+# This file is part of Hive Stripe API.
+#
+# Hive Stripe API is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
+#
+# Hive Stripe API is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License along with
+# Hive Stripe API. If not, see <http://www.apache.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__copyright__ = "Copyright (c) 2008-2025 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "Apache License, Version 2.0"
+""" The license for the module """
+
+import unittest
+
+
+class BaseTest(unittest.TestCase):
+
+    def test_basic(self):
+        self.assertEqual(1 + 1, 2)
